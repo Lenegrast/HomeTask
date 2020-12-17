7db1c1b2bf90ae5919189bc63a0e668be0cfcde3
Lenegrast <55450845+Lenegrast@users.noreply.github.com>
2020-12-17 22:24:47 +0500

    Do

---
+++ Maze.cpp
@@ -0,0 +1,146 @@
+#include "Maze.h"
+#include "MCell.h"
+using namespace std;
+
+
+bool Maze::hasConnection(int i1, int j1, int i2, int j2)
+{
+	if (!cellMaze(i1, j1) || !cellMaze(i2, j2)) return false; 
+
+	if (j1 < j2)
+	{
+		return m_field[j1 * m_n + i1].maze_right;
+	}
+
+	else if (j1 > j2)
+	{
+		return m_field[j2 * m_n + i2].maze_right;
+	}
+
+	else if (i1 < i2)
+	{
+		return m_field[j1 * m_n + i1].maze_down;
+	}
+
+	else
+	{
+		return m_field[j2 * m_n + i2].maze_down;
+	}
+
+}
+
+void Maze::makeConnection(int i1, int j1, int i2, int j2)
+{
+	if (!cellMaze(i1, j1) || !cellMaze(i2, j2)) return;
+	
+	if (j1 < j2)
+	{
+		m_field[j1 * m_n + i1].maze_right = true;
+	}
+
+	else if (j1 > j2)
+	{
+		m_field[j2 * m_n + i2].maze_right = true;
+	}
+
+	else if (i1 < i2)
+	{
+		m_field[j1 * m_n + i1].maze_down = true;
+	}
+
+	else
+	{
+		m_field[j2 * m_n + i2].maze_down = true;
+	}
+};
+
+void Maze::removeConnection(int i1, int j1, int i2, int j2)
+{
+	if (!cellMaze(i1, j1) || !cellMaze(i2, j2))  return;
+	
+	if (j1 < j2)
+	{
+		m_field[j1 * m_n + i1].maze_right = false;
+	}
+
+	else if (j1 > j2)
+	{
+		m_field[j2 * m_n + i2].maze_right = false;
+	}
+
+	else if (i1 < i2)
+	{
+		m_field[j1 * m_n + i1].maze_down = false;
+	}
+
+	else
+	{
+		m_field[j2 * m_n + i2].maze_down = false;
+	}
+};
+
+Maze::Maze(int n, int m)
+{
+	m_field = new MCell[n * m];
+	m_n = n;
+	m_m = m;
+}
+
+const MCell& Maze::cell(int i, int j) const { return *m_field; }
+
+bool Maze::cellMaze(int i, int j) { return i >= 0 && i < m_n&& j >= 0 && j < m_m; }
+
+void Maze::printMaze()
+{
+	bool left = false;
+	bool up = false;
+	bool right = false;
+	bool down = false;
+	
+	for (int i = 0; i < m_n; i++)
+	{
+		for (int j = 0; j < m_m; j++)
+		{
+			if (hasConnection(i, j, i, j - 1))
+				left = true;
+			if (hasConnection(i, j, i - 1, j))
+				up = true;
+			if (hasConnection(i, j, i, j + 1))
+				right = true;
+			if (hasConnection(i, j, i + 1, j))
+				down = true;
+			if (left && up && right && down)
+				cout << (char)197;
+			else if (left && up && right)
+				cout << (char)193;
+			else if (left && up && down)
+				cout << (char)180;
+			else if (left && right && down)
+				cout << (char)194;
+			else if (up && right && down)
+				cout << (char)195;
+			else if (left && up)
+				cout << (char)217;
+			else if (up && right)
+				cout << (char)192;
+			else if (left && down)
+				cout << (char)191;
+			else if (right && down)
+				cout << (char)218;
+			else if (up && down)
+				cout << (char)179;
+			else if (left && right)
+				cout << (char)196;
+			else
+				cout << '0';
+
+			left = false;
+			up = false;
+			right = false;
+			down = false;
+		}
+		cout << "\n";
+	}
+}
+
+Maze::~Maze() { free(m_field); }
